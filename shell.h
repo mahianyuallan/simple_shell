b80be87bafad6985acbb37843b4493d387645f35
Leon <leonmuraguri@gmail.com>
2023-11-13 10:19:39 -0800

    Task done

--- shell.h
+++ shell.h
@@ -75,6 +75,68 @@ int _st_env(dt_shell *sh_data);
 int _unst_env(dt_shell *sh_data);
 
 
+/*  err_get.c */
+int err_get(dt_shell *sh_data, int val_err);
 
+
+/*  hlp_get.c  */
+int hlp_get(dt_shell *sh_data);
+
+
+
+/*  inbuilt_get.c  */
+int (*inbuilt_get(char *cmd))(dt_shell *sh_data);
+
+/* line_ex.c   */
+int line_ex(dt_shell *sh_data);
+
+
+/*  line_get.c  */
+void set_line(char **l_ptr, size_t *n, char *buff_size, size_t m);
+ssize_t line_get(char **l_ptr, size_t *n, FILE *stream);
+
+
+
+/*  line_rd.c  */
+char *line_rd(int *ret_val);
+
+
+
+/*  loop_shl.c  */
+char *comment_none(char *val);
+void loop_shl(dt_shell *sh_data);
+
+
+
+/*  replace_var.c  */
+void env_chp(l_var **h, char *in, dt_shell *data);
+int var_chp(l_var **h, char *in, char *st, dt_shell *data);
+char *input_rep(l_var **h, char *input, char *new_input, int len_b);
+char *rep_var(char *input, dt_shell *sh_data);
+
+
+/*  shell_ext.c  */
+int shell_exit(dt_shell *sh_data);
+
+
+
+/* sign_get.c   */
+void sign_get(int val);
+
+
+
+/*  split_cmd.c  */
+char *char_swp(char *in, int bool);
+void new_nodes(list_sep **head_s, list_line **head_l, char *in);
+void forward(list_sep **list_s, list_line **list_l, dt_shell *sh_data);
+int cmd_split(dt_shell *sh_data, char *in);
+char **line_split(char *in);
+
+
+/*  str_to_var.c  */
+void env_chk(l_var **h, char *in, dt_shell *data);
+int var_chk(l_var **h, char *in, char *st, dt_shell *data);
+char *input_rep(l_var **head, char *in, char *new_input, int nlen);
+char *var_rep(char *input, dt_shell *sh_data);
 
 #endif
