20c6a072726420796519855d854055b3e79d875d
Allan Mwenja <140613367+mahianyuallan@users.noreply.github.com>
2023-11-14 09:28:20 +0300

    Update

--- alist.c
+++ alist.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * add_sep_node_end - adds a separator found at the end
- * of a sep_list.
- * @head: head of the linked list.
- * @sep: separator found (; | &).
- * Return: address of the head.
+ * add_sep_node_end - inputs a separator and it is found at the end of the line
+ * of list_sep.
+ * @head: this is the head of the list.
+ * @sep: contains the seperators (; | &).
+ * Return: it returns the main address of the head.
  */
-sep_list *add_sep_node_end(sep_list **head, char sep)
+list_sep *add_sep_node_end(list_sep **head, char sep)
 {
-	sep_list *new, *temp;
+	list_sep *new, *temp;
 
-	new = malloc(sizeof(sep_list));
+	new = malloc(sizeof(list_sep));
 	if (new == NULL)
 		return (NULL);
 
@@ -34,14 +34,14 @@ sep_list *add_sep_node_end(sep_list **head, char sep)
 }
 
 /**
- * free_sep_list - frees a sep_list
- * @head: head of the linked list.
- * Return: no return.
+ * free_list_sep - frees a list_sep
+ * @head: this is the head of the list.
+ * Return: null.
  */
-void free_sep_list(sep_list **head)
+void free_list_sep(list_sep **head)
 {
-	sep_list *temp;
-	sep_list *curr;
+	list_sep *temp;
+	list_sep *curr;
 
 	if (head != NULL)
 	{
@@ -56,11 +56,11 @@ void free_sep_list(sep_list **head)
 }
 
 /**
- * add_line_node_end - adds a command line at the end
+ * add_line_node_end - it adds a command line
  * of a line_list.
- * @head: head of the linked list.
- * @line: command line.
- * Return: address of the head.
+ * @head: this is the head of the list.
+ * @line: this is the command line that is to be added at the end.
+ * Return: it returns the main address of the head.
  */
 line_list *add_line_node_end(line_list **head, char *line)
 {
@@ -90,8 +90,8 @@ line_list *add_line_node_end(line_list **head, char *line)
 
 /**
  * free_line_list - frees a line_list
- * @head: head of the linked list.
- * Return: no return.
+ * @head: this is head of the list.
+ * Return: null.
  */
 void free_line_list(line_list **head)
 {
