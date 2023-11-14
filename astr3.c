79f5803771939bc20edcf6e6b4d0ef707cd63bed
Allan Mwenja <140613367+mahianyuallan@users.noreply.github.com>
2023-11-14 10:09:50 +0300

    Update

--- astr3.c
+++ astr3.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * rev_string - reverses a string.
- * @s: input string.
- * Return: no return.
+ * rev_string - the function reverses a string.
+ * @s: a string.
+ * Return: null.
  */
 void rev_string(char *s)
 {
